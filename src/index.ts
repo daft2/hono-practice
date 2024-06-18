1472f6c409071fbec3498faf8cd01ef6d18b41e6
Daft <daftdevs@gmail.com>
2024-06-18 19:25:40 +0700

    add medium task route in index

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { Hono } from "hono";
 import EasyRoutes from "./domain/easy";
+import MediumRoutes from "./domain/medium";
 
 const app = new Hono();
 
 app.route("/easy", EasyRoutes);
+app.route("/medium", MediumRoutes);
 
 export default app;
